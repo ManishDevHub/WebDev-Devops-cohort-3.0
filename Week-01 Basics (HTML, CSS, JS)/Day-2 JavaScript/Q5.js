ea0a744c47bb47fa3ad2d5ffa8ebcbdb8050bc78
Manish Kumar <ramuk13476@gmail.com>
2025-06-16 21:11:35 +0530

    Update Q5.js

--- Week-01 Basics (HTML, CSS, JS)/Day-2 JavaScript/Q5.js
+++ Week-01 Basics (HTML, CSS, JS)/Day-2 JavaScript/Q5.js
@@ -3,3 +3,12 @@ function sum(n){
     return (n * (n + 1)) / 2;
 }
 console.log(sum(5));
+
+Write a function to find sum of two number and print 
+
+function sumNum(a,b){
+return a+b;
+
+console.log( sum(2 + 3));
+
+}
